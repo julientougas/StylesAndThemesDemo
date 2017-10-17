e621308315af755269f855fa7c0583b1c2d751ab
julientougas <toug0008@algonquinlive.com>
2017-10-17 16:07:27 -0400

    Module 6 finished

--- app/src/main/java/com/sriyanksiddhartha/stylesandthemesdemo/MainActivity.java
+++ app/src/main/java/com/sriyanksiddhartha/stylesandthemesdemo/MainActivity.java
@@ -1,10 +1,14 @@
 package com.sriyanksiddhartha.stylesandthemesdemo;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 
@@ -26,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
 		LinearGradient linearGradient = new LinearGradient(0, 0, 0, txvGradient.getTextSize(), Color.CYAN, Color.BLUE, Shader.TileMode.MIRROR);
 		txvGradient.getPaint().setShader(linearGradient);
 
+	}
+
+	public void moveToSecondActivity(View view) {
+		Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this);
+			startActivity(intent, options.toBundle());
+		} else {
+			startActivity(intent);
+		}
 	}
 }
